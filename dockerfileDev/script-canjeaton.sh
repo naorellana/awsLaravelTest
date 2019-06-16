3f2b5c1702636f7d1d910a28efb4e8c54422de54
Nery Alexis Orellana <norellanac@miumg.edu.gt>
2019-06-16 14:50:31 -0600

    Dockerfile par entorno de pruebas y producion aws

---
+++ dockerfileDev/script-canjeaton.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#instalar dependencias de proyecto y configuracion de aranque de proyecto
+cd /var/www/html/app
+composer install
+cp .env.example .env
+php artisan key:generate
+# Set group permissions
+chgrp -R www-data /var/www/html/app/*
+# Set folder permissions
+chmod -R 775 /var/www/html/app/storage/*
+apachectl -DFOREGROUND
+#CMD apachectl -DFOREGROUND
