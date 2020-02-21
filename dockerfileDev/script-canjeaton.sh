9057a7b5d69244a8dd64aaf6fac61078f79503ab
Nery Orellana <norellanac@10x.org>
2020-02-21 17:38:05 -0600

    contenedor php, con apache

--- dockerfileDev/script-canjeaton.sh
+++ dockerfileDev/script-canjeaton.sh
@@ -2,12 +2,15 @@
 
 #instalar dependencias de proyecto y configuracion de aranque de proyecto
 cd /var/www/html/app
-#composer install
-#cp .env.example .env
+composer install
+cp .env.example .env
 #php artisan key:generate
 # Set group permissions
 chgrp -R www-data /var/www/html/app/*
 # Set folder permissions
-#chmod -R 775 /var/www/html/app/storage/*
+chmod -R 775 /var/www/html/app/storage/*
 apachectl -DFOREGROUND
 #CMD apachectl -DFOREGROUND
+#******como crear imagen y levantar contenedo
+#docker run -it --name=BI10xProdContainer -p 3000:80 -v /home/norellanac/Documentos/volDockerContainers/laravel/10xBI:/var/www/html/app/ norellana/apache-composer
+#sudo docker build  -t  norellana/apache-composer .
